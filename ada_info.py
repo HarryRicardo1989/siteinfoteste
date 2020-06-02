4e2563525bcdad222bb604cff08255fc80e9e397
Ricardo Paschoali <ricardo.paschoali@gmail.com>
2020-06-02 10:18:04 -0300

    Site Funcionando inicio

---
+++ ada_info.py
@@ -0,0 +1,46 @@
+import requests
+import json
+
+
+class AdaInfo:
+    def __init__(self):
+        self.__status = ''
+        self.__passagens = ''
+        pass
+
+    def get_passagens(self):
+        list_passagens = [['Nome da ADA: ET-CSS-001 - OFFLINE'], ['Nome da ADA: ET-CSS-002 - OFFLINE'], ]
+        try:
+            estacao = self.__get_url()[0]
+            list_passagens[0] =requests.get(f'{estacao}/passagens').text.replace('"', '').replace(
+                        '\n', '').replace('{', '').replace('}', '').split(',')
+        except:
+            pass
+        try:
+            estacao = self.__get_url()[1]
+            list_passagens[1] = requests.get(f'{estacao}/passagens').text.replace('"', '').replace(
+                    '\n', '').replace('{', '').replace('}', '').split(',')
+        except:
+            pass
+        return list_passagens
+
+    def __get_url(self):
+        lista_url = []
+        with open(f'/var/local/ada-urls.txt') as ada_urls:
+            for url in ada_urls:
+                lista_url.append(url.strip('\n'))
+        return lista_url
+
+    def get_status_dic(self):
+        list_status = [{'Nome da ADA': 'OFFLINE', 'Temperatura do Processador':'0.0' ,'Porcentagem do SSD usado':'0.0', 'Posicao Atual da Antena':'000'},{'Nome da ADA': 'OFFLINE', 'Temperatura do Processador':'0.0' ,'Porcentagem do SSD usado':'0.0', 'Posicao Atual da Antena':'000'}]
+        try:
+            estacao = self.__get_url()[0]
+            list_status[0] = requests.get(f'{estacao}/status').json()
+        except:
+            pass
+        try:    
+            estacao = self.__get_url()[1]
+            list_status[1] = requests.get(f'{estacao}/status').json()
+        except:
+            pass
+        return list_status
---
+++ siteInfo.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+from flask import Flask, render_template, request, json
+from ada_info import AdaInfo
+from formata_status import FormataStatus
+app = Flask(__name__)
+
+
+@app.route('/')
+def Index():
+    passagens_et1 = []
+    status_et1 = []
+    passagens_et2 = []
+    status_et2 = []
+    try:
+        passagens_et1 = AdaInfo().get_passagens()[0]
+        status_et1 = FormataStatus().formata_status()[0]
+        passagens_et2 = AdaInfo().get_passagens()[1]
+        status_et2 = FormataStatus().formata_status()[1]
+    except:
+        pass
+
+    return render_template('ada_info.html', status_et1=status_et1, passagens_et1=passagens_et1, status_et2=status_et2, passagens_et2=passagens_et2)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=81, debug=False, threaded=True )  
