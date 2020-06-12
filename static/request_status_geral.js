4edc47bde81c74b528c590554c2735a105736cd3
ricardopaschoali <ricardopaschoali@ET-CSS-DEV>
2020-06-11 23:51:52 -0300

    versao ok Func

--- static/request_status_geral.js
+++ static/request_status_geral.js
@@ -1,20 +1,9 @@
-document.querySelector(".titulo-principal").textContent = "Status do Sistema";
-var div, li, ul, tag_Name;
-function montaUl(liItens, classe) {
-    div = document.createElement("div");
-    div.classList.add('bloco')
-    ul = document.createElement("ul");
-    ul.classList.add(classe)
-    div.appendChild(ul);
-    liItens.forEach(function (item) {
-        li = document.createElement("li");
-        li.innerHTML = item;
-        ul.appendChild(li);
-    });
-    return div
-}
+document.querySelector(".titulo-principal").textContent = "Status DATASAT";
+const divPrincipal = document.querySelector("#InicioBlocos");
+var ulEt01, pass;
+
 function createUlList(lista, classeUL) {
-    ul = document.createElement("ul");
+    let ul = document.createElement("ol");
     ul.classList.add(classeUL)
     lista.forEach(function (item) {
         li = document.createElement("li");
@@ -26,33 +15,32 @@ function createUlList(lista, classeUL) {
 }
 
 function divCreate(tagFilha, classeDiv) {
-    div = document.createElement("div");
+    let div = document.createElement("div");
     div.classList.add(classeDiv);
     div.appendChild(tagFilha);
     return div
 }
 
-function tagCreate(tagText) {
-    tag_Name = document.createElement("h1");
-    tag_Name.classList.add("");
-    tag_Name.innerHTML = tagText;
+function appendTag(tagMain, tagToAppend) {
+    tagMain.appendChild(tagToAppend);
+}
+
+function h1Create(h1Text, className) {
+    let tag_Name = document.createElement("h1");
+    tag_Name.classList.add(className);
+    tag_Name.innerHTML = h1Text;
     return tag_Name
 }
 function formataPassagens(lista) {
-    listaFormatada = []
+    let listaFormatada = []
+    listaFormatada.push(`<h2><span class="hora">Hora</span>&#8658<span class="sat">Satelite</span></h2>`)
     for (x of Object.keys(lista)) {
-        listaFormatada.push(`${x}  -  ${lista[x]}`)
+        listaFormatada.push(`<span class="hora">${x}</span>&#8658;<span class="sat">${lista[x]}</span>`)
     }
     return listaFormatada
 }
-function formataStatus(status) {
-    var statusFormatado = []
-    statusFormatado.push("Hora Server: " + status['Hora_Atual']);
-    statusFormatado.push("Porcentagem do SSD usado: " + status['SSD_used']);
-    statusFormatado.push("Temperatura do Processador: " + status['Temp_CPU'] + "ºC");
-    statusFormatado.push("Posição da Antena: " + status['Posicao_Atual']);
-    return statusFormatado
-}
+
+
 function concatLists(listMain, listaSec) {
 
     for (item of Object.keys(listaSec)) {
@@ -63,19 +51,25 @@ function concatLists(listMain, listaSec) {
 
 function FetchParser(jsonObj) {
 
-    var statusGeral = jsonObj["statusSistema"];
-    var passagens = statusGeral["Passagens"];
-    var statusSistema = statusGeral["Status"];
-    var agendaEt1 = formataPassagens(passagens[1]["Passagens ET-CSS-001"]);
-    var agendaEt2 = formataPassagens(passagens[2]["Passagens ET-CSS-002"]);
-    var statusEt1 = formataStatus(statusSistema[1]["Status ET-CSS-001"]);
-    var statusEt2 = formataStatus(statusSistema[2]["Status ET-CSS-002"]);
+    let statusGeral = jsonObj["statusSistema"];
+    let passagens = statusGeral["Passagens"];
+    let statusSistema = statusGeral["Status"];
+    let agendaEt1 = formataPassagens(passagens[1]["Passagens ET-CSS-001"]);
+    let agendaEt2 = formataPassagens(passagens[2]["Passagens ET-CSS-002"]);
+    let statusEt1 = formataStatus(statusSistema[1]["Status ET-CSS-001"]);
+    let statusEt2 = formataStatus(statusSistema[2]["Status ET-CSS-002"]);
 
-    var divPrincipal = document.querySelector("#InicioBlocos");
+
     divPrincipal.innerHTML = '';
-    divPrincipal.appendChild(montaUl(concatLists(statusEt1, agendaEt1), "pass"));
-    divPrincipal.appendChild(montaUl(concatLists(statusEt2, agendaEt2), "pass"));
-    divPrincipal.appendChild(createUlList(agendaEt1, "Bloco"))
+    divPrincipal.appendChild(divCreate(divCreate(divCreate(createUlList(statusEt1, "status"), "ET-CSS-001"), "datasat"), "bloco"))
+    divPrincipal.appendChild(divCreate(divCreate(divCreate(createUlList(statusEt2, "status"), "ET-CSS-002"), "datasat"), "bloco"))
+    ulEt01 = document.querySelector(".ET-CSS-001");
+    ulEt01.insertAdjacentHTML('beforebegin', '<h1>ET-CSS-001</h1>');
+    ulEt01.appendChild(createUlList(agendaEt1, "pass"), "pass");
+    ulEt01 = document.querySelector(".ET-CSS-002");
+    ulEt01.appendChild(createUlList(agendaEt2, "pass"), "pass");
+    ulEt01.insertAdjacentHTML('beforebegin', '<h1>ET-CSS-002</h1>');
+
 
 
 }
@@ -97,8 +91,6 @@ function atualizaStatus() {
 
 };
 
-
 setInterval(function () {
     atualizaStatus();
-
-}, 5000)
+}, 1000)
---
+++ static/status.js
@@ -0,0 +1,28 @@
+function formataStatus(status) {
+    let statusFormatado = []
+    /* classificaçao Hora */
+    statusFormatado.push(`Hora Server: <span class="verde">${status['Hora_Atual']}</span>`);
+    /* classificaçao SSD */
+    if (status['SSD_used'] < "50%") {
+        statusFormatado.push(`Porcentagem do SSD usado: <span class="verde">${status['SSD_used']}</span>`);
+    } else if (status['SSD_used'] >= "50%" || status['SSD_used'] <= "80%") {
+        statusFormatado.push(`Porcentagem do SSD usado: <span class="amarelo">${status['SSD_used']}</span>`);
+    } else if (status['SSD_used'] < "80%") {
+        statusFormatado.push(`<span class="vermelho">Porcentagem do SSD usado: ${status['SSD_used']}</span>`);
+    }
+    /* classificaçao Temperatura Processador */
+    if (status['Temp_CPU'] < "+65.0") {
+        statusFormatado.push(`Temperatura do Processador:  <span class="verde">${status['Temp_CPU']}ºC</span>`);
+    } else if (status['Temp_CPU'] >= "+65.0" || status['Temp_CPU'] <= "+75.0") {
+        statusFormatado.push(`Temperatura do Processador: <span class="amarelo"> ${status['Temp_CPU']}ºC</span>`);
+    } else if (status['Temp_CPU'] > "+75.0") {
+        statusFormatado.push(`<span class="vermelho">Temperatura do Processador:  ${status['Temp_CPU']}ºC</span>`);
+    }
+    /* classificaçao posiçao */
+    if (status['Posicao_Atual'] == "AZ010.00 EL-00.00" || status['Posicao_Atual'] == "AZ075.00 EL-00.00" || status['Posicao_Atual'] == "AZ010.00 EL00.00" || status['Posicao_Atual'] == "AZ075.00 EL00.00") {
+        statusFormatado.push(`Posição da Antena:  <span class="verde">${status['Posicao_Atual']}</span>`);
+    } else {
+        statusFormatado.push(`Posição da Antena:  <span class="amarelo">${status['Posicao_Atual']}</span>`);
+    }
+    return statusFormatado
+}
