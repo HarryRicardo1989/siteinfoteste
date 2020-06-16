9083c9a8042729a1ddcbea0744ed1763068b9f66
Ricardo Paschoali <ricardo.paschoali@gmail.com>
2020-06-16 18:07:07 -0300

    refatoraçao

--- siteInfo.py
+++ siteInfo.py
@@ -1,11 +1,13 @@
 #!/bin/python3
-from flask import Flask, render_template, request, json, redirect, url_for
-from ada_info import AdaInfo
-from formata_status import FormataStatus
-from status_geral import StatusSistemasAda
-
+from flask import Flask, render_template, redirect, url_for
+from time import sleep
+# thread
+import threading
 from statusGeral import StatusSistemas
+
+
 app = Flask(__name__)
+status_geral_atualiza = None
 
 
 @app.route('/')
@@ -15,34 +17,27 @@ def index():
 
 @ app.route('/site/servicos/datasat/status')
 def status():
-    passagens_et1 = []
-    status_et1 = []
-    passagens_et2 = []
-    status_et2 = []
-    try:
-        passagens_et1 = AdaInfo().get_passagens()[0]
-        status_et1 = FormataStatus().formata_status()[0]
-        passagens_et2 = AdaInfo().get_passagens()[1]
-        status_et2 = FormataStatus().formata_status()[1]
-    except:
-        pass
 
-    return render_template('ada_info.html', status_et1=status_et1, passagens_et1=passagens_et1, status_et2=status_et2, passagens_et2=passagens_et2)
-
-
-@ app.route('/statusSistema2')
-def status_sistema():
-    status_geral = StatusSistemasAda()
-
-    return status_geral.get_statusgeral()
+    return render_template('ada_info.html')
 
 
 @ app.route('/statusSistema')
-def status_sistema2():
+def status_sistema():
     status_geral2 = StatusSistemas()
 
-    return status_geral2.get_statusgeral()
+    return status_geral_atualiza
+
+
+def atualiza_status():
+    global status_geral_atualiza
+    while(True):
+        status_geral2 = StatusSistemas()
+
+        status_geral_atualiza = status_geral2.get_statusgeral()
+        sleep(1)
 
 
 if __name__ == '__main__':
+    threadstatus = threading.Thread(target=atualiza_status)
+    threadstatus.start()
     app.run(host='0.0.0.0', port=81, debug=False, threaded=True)
