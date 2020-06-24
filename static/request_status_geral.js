026cf738f275968780bf575799eb40eeea2192e8
Ricardo Paschoali <ricardo.paschoali@gmail.com>
2020-06-23 23:16:23 -0300

    NoSatScheduled fixed

--- static/request_status_geral.js
+++ static/request_status_geral.js
@@ -1,7 +1,7 @@
 document.querySelector(".titulo-principal").textContent = "Status DATASAT";
 const divPrincipal = document.querySelector("#InicioBlocos");
 var ulEt01, pass;
-
+var movimenta = false
 function createUlList(lista, classeUL) {
     let ul = document.createElement("ol");
     ul.classList.add(classeUL)
@@ -28,13 +28,35 @@ function h1Create(h1Text, className) {
     return tag_Name
 }
 
-function formataPassagens(lista) {
+function formataPassagens(lista, idName) {
     let listaFormatada = []
-    listaFormatada.push(`<h2><span class="hora">Hora</span>&#8658<span class="sat">Satelite</span></h2>`)
+    listaFormatada.push(`<h2><span class="hora">Hora </span><span class="sat">   Satelite</span></h2>`)
     for (x of Object.keys(lista)) {
         listaFormatada.push(`<span class="hora">${x}</span>&#8658;<span class="sat">${lista[x]}</span>`)
     }
+    if (movimenta == true) {
+        listaFormatada[1] = listaFormatada[1].replace(/class=/g, `id="${idName}" class=`)
+    } else {
+        listaFormatada[1] = listaFormatada[1].replace(/class=/g, `id="${idName}" class=`)
+    }
     return listaFormatada
+}
+
+function movimento(status) {
+    if (status['Posicao_Atual'] == "AZ010.00 EL-00.00" || status['Posicao_Atual'] == "AZ075.00 EL-00.00" || status['Posicao_Atual'] == "AZ010.00 EL00.00" || status['Posicao_Atual'] == "AZ075.00 EL00.00") {
+        return "parada"
+    } else {
+        return "movimentando"
+    }
+}
+
+function checkSheduleds(lista) {
+    if (Object.keys(lista).length) {
+        return lista
+    }
+    else {
+        return { "00:00": "NoSatScheduledYet" }
+    }
 }
 
 function FetchParser(jsonObj) {
@@ -42,12 +64,11 @@ function FetchParser(jsonObj) {
     let statusGeral = jsonObj["statusSistema"];
     let passagens = statusGeral["Passagens"];
     let statusSistema = statusGeral["Status"];
-    let agendaEt1 = formataPassagens(passagens[1]["Passagens ET-CSS-001"]);
-    let agendaEt2 = formataPassagens(passagens[2]["Passagens ET-CSS-002"]);
+    let agendaEt1 = formataPassagens(checkSheduleds(passagens[1]["Passagens ET-CSS-001"]), movimento(statusSistema[1]["Status ET-CSS-001"]));
+    let agendaEt2 = formataPassagens(checkSheduleds(passagens[2]["Passagens ET-CSS-002"]), movimento(statusSistema[2]["Status ET-CSS-002"]));
     let statusEt1 = formataStatus(statusSistema[1]["Status ET-CSS-001"]);
     let statusEt2 = formataStatus(statusSistema[2]["Status ET-CSS-002"]);
-
-    divPrincipal.innerHTML = ''
+    divPrincipal.innerHTML = '';
     divPrincipal.appendChild(divCreate(divCreate(divCreate(createUlList(statusEt1, "status"), "ET-CSS-001"), "datasat"), "bloco"))
     divPrincipal.appendChild(divCreate(divCreate(divCreate(createUlList(statusEt2, "status"), "ET-CSS-002"), "datasat"), "bloco"))
     ulEt01 = document.querySelector(".ET-CSS-001");
@@ -80,4 +101,4 @@ function atualizaStatus() {
 
 setInterval(function () {
     atualizaStatus();
-}, 1000)
+}, 300)
